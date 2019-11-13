7bdf580acab9d997deaa50b0555a5a25c853d087
a186r <hi.aewalle@gmail.com>
2019-11-13 15:38:14 +0800

    fix(minigrep): 修复冲突

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,7 @@
 use std::env;
 
-<<<<<<< HEAD
 //grep 获取一个文件名和一个字符串作为参数，接着读取文件并找到其中包含字符串参数的行，然后打印出这些行
-fn main() {
-//    读取任何传递给minigrep的命令行参数并将其收集到一个vector中
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-}
-=======
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -18,4 +12,3 @@ fn main() {
     println!("Searching for {}", query);
     println!("In file {}", filename);
 }
->>>>>>> fd4e3b40b9107bbee967f2b0395c920c19485d9e
