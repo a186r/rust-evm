95971a36d2a469c87ebfe8ec37db195dd10cb50b
a186r <hi.aewalle@gmail.com>
2019-11-25 17:44:03 +0800

    feat(iterators): 性能比较：循环对迭代器

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -17,4 +17,18 @@ fn main() {
     let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
 
     assert_eq!(v2, vec![2, 3, 4]);
+
+    let buffer: &mut [i32];
+    let coefficients: [i64; 12];
+    let qlp_shift: i16;
+
+    for i in 12..buffer.len(){
+        let prediction = coefficients.iter()
+            .zip(&buffer[i - 12..i])
+            .map(|(&c, &s)| c * s as i64)
+            .sum::<i64>() >> qlp_shift;
+
+        let delta = buffer[i];
+        buffer[i] = prediction as i32 + delta;
+    }
 }
