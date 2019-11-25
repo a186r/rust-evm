81dba590bca1ba64ab3db30044734d09c2a94f13
a186r <hi.aewalle@gmail.com>
2019-11-25 17:06:20 +0800

    feat(iterators): 迭代器

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -11,4 +11,10 @@ fn main() {
     assert_eq!(v1_iter.next(), Some(&2));
     assert_eq!(v1_iter.next(), Some(&3));
     assert_eq!(v1_iter.next(), None);
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
 }
