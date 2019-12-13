b60dd70ea335ff7ab273e2f58551657ca3722ec9
a186r <hi.aewalle@gmail.com>
2019-12-13 15:48:07 +0800

    feat(patterns): 所有可能用到模式的位置

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -1,30 +1,39 @@
+//fn main() {
+//    let favorite_color: Option<&str> = None;
+//    let is_tuesday = false;
+//    let age: Result<u8, _> = ("34").parse();
+//
+//    if let Some(color) = favorite_color{
+//        println!("Using your favorite color, {}, as the background", color);
+//    }else if is_tuesday{
+//        println!("Tuesday is green day!");
+//    }else if let Ok(age) = age{
+//        if age > 30 {
+//            println!("Using purple as the background color");
+//        }else{
+//            println!("Using orange as the background color");
+//        }
+//    }else{
+//        println!("Using blue as the background color");
+//    }
+//
+////    while let循环
+//    let mut stack = Vec::new();
+//    stack.push(1);
+//    stack.push(2);
+//    stack.push(3);
+//
+////    使用while let循环，只要stack.pop返回Some就打印出值
+//    while let Some(top) = stack.pop(){
+//        println!("{}", top);
+//    }
+//}
+
 fn main() {
-    let favorite_color: Option<&str> = None;
-    let is_tuesday = false;
-    let age: Result<u8, _> = ("34").parse();
+    let point = (3, 5);
+    print_coordinates(&point);
+}
 
-    if let Some(color) = favorite_color{
-        println!("Using your favorite color, {}, as the background", color);
-    }else if is_tuesday{
-        println!("Tuesday is green day!");
-    }else if let Ok(age) = age{
-        if age > 30 {
-            println!("Using purple as the background color");
-        }else{
-            println!("Using orange as the background color");
-        }
-    }else{
-        println!("Using blue as the background color");
-    }
-
-//    while let循环
-    let mut stack = Vec::new();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-
-//    使用while let循环，只要stack.pop返回Some就打印出值
-    while let Some(top) = stack.pop(){
-        println!("{}", top);
-    }
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
 }
