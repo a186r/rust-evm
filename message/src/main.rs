b5f70479a95d77507b7a6c5c1b8398825cef932e
a186r <hi.aewalle@gmail.com>
2019-12-17 18:03:05 +0800

    feat(message): 匹配嵌套的枚举

--- message/src/main.rs
+++ message/src/main.rs
@@ -1,25 +1,35 @@
+enum Color{
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
 enum Message{
     Quit,
     Move {x: i32, y: i32},
     Write(String),
-    ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
 }
 
 fn main() {
-    let msg = Message::ChangeColor(0, 160, 255);
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
-        Message::Quit => {
-            println!("The Quit....");
-        },
+//        Message::Quit => {
+//            println!("The Quit....");
+//        },
+//
+//        Message::Move {x, y} => {
+//            println!("Move in x {} and y {}", x, y);
+//        },
 
-        Message::Move {x, y} => {
-            println!("Move in x {} and y {}", x, y);
+//        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color and h, s, v is {}, {} and {}", h, s, v);
         },
-
-        Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => {
-            println!("Change color and r, g, b is {}, {} and {}", r, g, b);
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("r, g, b is {}, {} and {}", r, g, b);
         }
+
+        _ => {}
     }
 }
