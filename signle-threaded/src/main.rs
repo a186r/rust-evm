5170057e71253c0779107905277b7fd3ea690b27
a186r <hi.aewalle@gmail.com>
2019-12-18 16:45:37 +0800

    feat(signle-threaded): 重构代码，使得if和else块中只包含两个情况不同的代码

--- signle-threaded/src/main.rs
+++ signle-threaded/src/main.rs
@@ -22,21 +22,34 @@ fn handle_connection(mut stream: TcpStream) {
 //    匹配请求并区别处理/请求与其他请求
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get){
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }else{
-//        收到其他请求的时候直接返回404
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
+//    将状态和文件名设置为变量
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        let response = format!("{}{}", status_line, contents);
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+//    if buffer.starts_with(get){
+//        let contents = fs::read_to_string("hello.html").unwrap();
+//        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+//        stream.write(response.as_bytes()).unwrap();
+//        stream.flush().unwrap();
+//    }else{
+////        收到其他请求的时候直接返回404
+//        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+//        let contents = fs::read_to_string("404.html").unwrap();
+//
+//        let response = format!("{}{}", status_line, contents);
+//
+//        stream.write(response.as_bytes()).unwrap();
+//        stream.flush().unwrap();
+//    }
 
 //    let contents = fs::read_to_string("hello.html").unwrap();
 //
