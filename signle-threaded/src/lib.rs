ecaee8e289ab7603c740eaa11dd53b900a412b54
a186r <hi.aewalle@gmail.com>
2019-12-19 14:27:03 +0800

    feat(signle-threaded): 修改ThreadPool存放Worker实例而不是直接存放线程

--- signle-threaded/src/lib.rs
+++ signle-threaded/src/lib.rs
@@ -1,4 +1,13 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool{
+    works:Vec<Worker>,
+}
+
+struct Worker{
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
 
 //实现接口
 impl ThreadPool{
@@ -11,12 +20,32 @@ impl ThreadPool{
         ///
         /// 'new'函数在size为0时会panic
         assert!(size > 0);
-        ThreadPool
+        /// with_capacty预先分配空间
+        let mut works = Vec::with_capacity(size);
+        for id in 0..size{
+//            创建线程并将他们存储在vector中
+            works.push(Worker::new(id));
+        }
+
+        ThreadPool{
+            works
+        }
     }
+
     pub fn execute<F>(&self, f: F)
         where
             F: FnOnce() + Send + 'static
     {
 
+    }
+}
+
+impl Worker{
+    pub fn new(id: usize) -> Worker{
+        let thread = thread::spawn(|| {});
+        Worker{
+            id,
+            thread,
+        }
     }
 }
