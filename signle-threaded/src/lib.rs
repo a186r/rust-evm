a9166b40daede8eeea9bb196935926489e0ffddc
a186r <hi.aewalle@gmail.com>
2019-12-19 16:27:14 +0800

    feat(signle-threaded): 将单线程server变为多线程server

--- signle-threaded/src/lib.rs
+++ signle-threaded/src/lib.rs
@@ -6,7 +6,19 @@ pub struct ThreadPool{
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+trait FnBox{
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
+
+//struct Job;
+//将Job改造成类型别名,类型别名能将长的类型变短
+type Job = Box<FnBox + Send + 'static>;
 
 struct Worker{
     id: usize,
@@ -43,15 +55,24 @@ impl ThreadPool{
         where
             F: FnOnce() + Send + 'static
     {
-
+        let job = Box::new(f);
+//        在通道中发出任务
+        self.sender.send(job).unwrap();
     }
 }
 
 impl Worker{
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker{
 //        空闭包创建一个thread
-        let thread = thread::spawn(|| {
-            receiver;
+//    我们希望闭包一直循环，向通道的接收端请求任务，并在得到任务时执行他们
+        let thread = thread::spawn(move || {
+            loop {
+//                首先调用locker来获取互斥器，如果锁定了互斥器，接着调用recv从通道中接收Job。
+//                调用recv会阻塞当前线程，所以如果还没有任务，其会等待知道有可用的任务。
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {} got a job; executing.", id);
+                job.call_box();
+            }
         });
 
         Worker{
